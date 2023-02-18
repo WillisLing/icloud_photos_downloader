b30cee98cd5b45a40af2cbe4a87b6206245d3120
willisling <willisling@live.com>
2023-02-18 11:04:14 +0800

    Fix icloud China feature

--- icloudpd/authentication.py
+++ icloudpd/authentication.py
@@ -35,7 +35,7 @@ def authenticator(domain):
                     client_id=client_id,
                     )
                 break
-            except pyicloud.exceptions.NoStoredPasswordAvailable:
+            except pyicloud.exceptions.PyiCloudNoStoredPasswordAvailableException:
                 # Prompt for password if not stored in PyiCloud's keyring
                 password = click.prompt("iCloud Password", hide_input=True)
 
@@ -44,8 +44,7 @@ def authenticator(domain):
                 raise TwoStepAuthRequiredError(
                     "Two-step/two-factor authentication is required!"
                 )
-            logger.info("Two-step/two-factor authentication is required!")
-            request_2sa(icloud, logger)
+        two_factor_authenticate(icloud, logger)
         return icloud
     return authenticate_
 
@@ -96,3 +95,43 @@ def request_2sa(icloud, logger):
         "the two-step authentication expires.\n"
         "(Use --help to view information about SMTP options.)"
     )
+
+def two_factor_authenticate(icloud, logging):
+    if icloud.requires_2fa:
+        logging.info("Two-factor authentication required.")
+        code = input("Enter the code you received of one of your approved devices: ")
+        result = icloud.validate_2fa_code(code)
+        logging.info("Code validation result: %s" % result)
+
+        if not result:
+            logging.error("Failed to verify security code")
+            sys.exit(1)
+
+        if not icloud.is_trusted_session:
+            logging.warning("Session is not trusted. Requesting trust...")
+            result = icloud.trust_session()
+            logging.info("Session trust result %s" % result)
+
+            if not result:
+                logging.error(
+                    "Failed to request trust. You will likely be prompted for the code again in the coming weeks")
+    elif icloud.requires_2sa:
+        logging.info("Two-step authentication required. Your trusted devices are:")
+
+        devices = icloud.trusted_devices
+        for i, device in enumerate(devices):
+            logging.info(
+                "  %s: %s" % (i, device.get('deviceName',
+                                            "SMS to %s" % device.get('phoneNumber')))
+            )
+
+        device = click.prompt('Which device would you like to use?', default=0)
+        device = devices[device]
+        if not icloud.send_verification_code(device):
+            logging.error("Failed to send verification code")
+            sys.exit(1)
+
+        code = click.prompt('Please enter validation code')
+        if not icloud.validate_verification_code(device, code):
+            logging.info("Failed to verify verification code")
+            sys.exit(1)
